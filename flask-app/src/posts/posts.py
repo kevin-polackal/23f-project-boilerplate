cf5738ffe3f5e886588a3f9889875af8cd1c5b40
jarenadams21 <jarenadams3@gmail.com>
2023-12-04 18:42:55 -0500

    joined posts on user id to match posts to users based on id

--- flask-app/src/posts/posts.py
+++ flask-app/src/posts/posts.py
@@ -19,7 +19,7 @@ def get_posts():
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for a list of posts
-    cursor.execute('SELECT userName, title, content FROM Post JOIN User LIMIT 50')
+    cursor.execute('SELECT User.userName, Post.title, Post.content FROM Post JOIN User ON Post.userID = User.userID LIMIT 50')
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
