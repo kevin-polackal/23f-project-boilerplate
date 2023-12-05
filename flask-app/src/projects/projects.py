a3963824d4af89926af0ead492139ff201fa2e40
Kevin Polackal <polackal.k@northeastern.edu>
2023-12-05 00:28:56 -0500

    pushing new changes

---
+++ flask-app/src/projects/projects.py
@@ -0,0 +1,174 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from src import db
+
+
+projects = Blueprint('projects', __name__)
+
+# VARIABLES:
+    # -> Overview
+    # -> Title
+    # -> Funding
+    # -> (primary key) ID
+
+# GET all the projects from the database with their title and description
+@projects.route('/projects', methods=['GET'])
+def get_projects():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of projects
+    cursor.execute('SELECT projectID, title, overview as description, funding FROM Project')
+
+    # grab the column headers from the returned data. Should be projectID, title, overview (desc), and funding
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # Append each project row to the json data to return
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Create or POST a new project
+@projects.route('/projects', methods=['POST'])
+def add_new_project():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variables of a project:
+    description = the_data['overview']
+    title = the_data['title']
+    funding = the_data['funding']
+
+    # Constructing the project query
+    query = 'INSERT INTO Project (overview, title, funding) VALUES ("'
+    query += description + '", "'
+    query += title + '", '
+    query += str(funding) + ')'
+    # Verify the project is in good form
+    current_app.logger.info(query)
+
+
+    # Execute and create the project in the database
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+
+@projects.route('/projects/join', methods=['POST'])
+def join_project():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    projectID = the_data['projectID']
+    userID = the_data['userID']
+    role = the_data['role']
+
+    # Constructing the query
+    query = 'INSERT INTO Role (projectID, userID, role) VALUES ("'
+    query += str(projectID) + '", "'
+    query += str(userID) + '", "'
+    query += role + '")'
+    current_app.logger.info(query)
+
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+
+# UPDATE/PUT a project based on its ID
+
+@projects.route('/projects/<projectID>', methods=['PUT'])
+def update_proj(projectID):
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    description = the_data['overview']
+    title = the_data['title']
+    funding = the_data['funding']
+
+    # Constructing the query
+    query = "UPDATE Project SET overview = '{}', title = '{}', funding = {} WHERE projectID = {}".format(description.replace("'", "''"), title.replace("'", "''"), funding, projectID)
+    # Verify query before sending to DB
+    current_app.logger.info(query)
+
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+
+# Delete a project based on its ID.
+@projects.route('/projects/<projectID>', methods=['DELETE'])
+def delete_proj(projectID):
+    
+    # executing and committing the deletion statement 
+    cursor = db.get_db().cursor()
+    cursor.execute('DELETE FROM Project WHERE projectID = {0}'.format(projectID))
+    db.get_db().commit()
+    
+    return 'Success!'
+
+# GET the milestones for a project based on its ID.
+@projects.route('/projects/<projectID>/milestones', methods=['GET'])
+def get_milestones(projectID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of projects
+    cursor.execute('SELECT projectID, dueDate, completed FROM Milestone WHERE Milestone.projectID = {0}'.format(projectID))
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# Get specific information regarding a project
+@projects.route('/projects/<projectID>', methods=['GET'])
+def get_project(projectID):
+    cursor = db.get_db().cursor()
+    # Prone to SQL injection, but okay for purposes of MVP
+    cursor.execute('SELECT title, overview, funding FROM Project WHERE projectID = {0}'.format(projectID))
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
